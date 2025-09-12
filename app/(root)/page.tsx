f6028de23d69ce74d69ece33f02cfa6c0662a153
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-09-12 23:07:43 +0500

    fix:the yellow line issues in the hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <>
       <section
-        className="pink_container"
+        className="pink_container "
         style={{
           backgroundImage:
-            "linear-gradient(to right, transparent 49.5%, rgba(251, 232, 67, 0.2) 49.5%, rgba(251, 232, 67, 0.6) 50.5%, transparent 50.5%)",
+            "linear-gradient(to right, transparent 49%, rgba(251, 232, 67, 0.2) 49%, rgba(251, 232, 67, 0.6) 51%, transparent 51%)",
           backgroundSize: "5% 100%",
           backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
+          backgroundRepeat: "repeat-x",
         }}
       >
         <h1 className="heading">
